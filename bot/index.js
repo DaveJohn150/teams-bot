4bf7add686d75c04b946c064d881d1161126f865
DaveJohn150 <davejohn150@gmail.com>
2022-08-16 10:33:00 +0800

    hive webhook integration auth

--- bot/index.js
+++ bot/index.js
@@ -14,58 +14,51 @@ server.listen(process.env.port || process.env.PORT || 3978, () => {
 
 // HTTP trigger to send notification. You need to add authentication / authorization for this API. Refer https://aka.ms/teamsfx-notification for more details.
 server.post(
-  "/api/notification",
-  restify.plugins.queryParser(),
-  restify.plugins.bodyParser(),
-  async (req, res) => {
-    try{
-    if (!req.headers.apikey){
-      throw 'Missing API header';
-    }
-    else if (req.headers.apikey != process.env.BOTAPI){ //make it check if the api key matches wherever they are stored
-      throw 'Invalid API key';
-    }
-      //maybe check if they have a key n stuff that would be good stuff
-    for (const target of await bot.notification.installations()) {
-      console.log(req.body)
-      message = req.body //azure receives them as actual JSON
-      await target.sendAdaptiveCard(
-        AdaptiveCards.declare(notificationTemplate).render({
-          title: message.title,
-          appName: message.appName,
-          description: message.description
-        })
-      );
-    }
-  }
-  catch (err){
-    console.log(err)
-    if (err == 'Missing API header'){
-      res.json(400, {errorMessage: 'Missing apikey header'});
-      return;
-    }
-    else if (err == 'Invalid API key'){
-      res.json(401, {errorMessage: 'Invalid API key'});
-      return;
-    }
-    else{
-    res.json(400, {errorMessage:  
-      'Requires JSON {title: "title of notification", appName: "name of origin app", description: "description of notification""}'});
-      return;
-    }
-  }
-    res.json(200, {});
-  }
-);
-
-server.post(
   "/api/alert",
   restify.plugins.queryParser(),
   restify.plugins.bodyParser(),
   async (req, res) => {
     try{console.log(req.body)}
     catch (err){console.log(err)}
-  })
+    try{
+    if (!req.headers.auth){
+      throw 'Missing auth header';
+    }
+    else if (req.headers.auth != process.env.BOTAUTH){ //make it check if the api key matches wherever they are stored
+      throw 'Invalid API key';
+    }
+      //maybe check if they have a key n stuff that would be good stuff
+    for (const target of await bot.notification.installations()) {
+      console.log(req.body)
+      message = req.body
+      await target.sendAdaptiveCard(
+        AdaptiveCards.declare(notificationTemplate).render({
+          title: message.title,
+          appName: message.appName,
+          description: message.content
+        })
+      );
+    }
+  }
+  catch (err){
+    console.log(err)
+    if (err == 'Missing auth header'){
+      res.json(400, {errorMessage: 'Missing auth header'});
+      return;
+    }
+    else if (err == 'Invalid API key'){
+      res.json(401, {errorMessage: 'Invalid API auth key'});
+      return;
+    }
+    else{
+    res.json(400, {errorMessage:  
+      'Requires JSON {title: "title of notification", appName: "name of origin app", content: "description of notification""}'});
+      return;
+    }
+  }
+    res.json(200, {});
+  }
+);
 
 // Message handler.
 server.post("/api/messages", async (req, res) => {
